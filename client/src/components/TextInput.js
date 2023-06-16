1f2385477cc2e0cbcc558fa4ead79bad8b58a98c
rajashilan <rajashilan07@gmail.com>
2023-06-16 15:50:11 +0800

    Feat(frontend): TextInput includes textarea

--- client/src/components/TextInput.js
+++ client/src/components/TextInput.js
@@ -3,22 +3,52 @@ import clsx from "clsx";
 import ErrorLabel from "./ErrorLabel";
 
 function TextInput(props) {
-  const { type, className, id, errors, register, value, ...rest } = props;
+  const {
+    type,
+    className,
+    id,
+    errors,
+    register,
+    value,
+    textarea,
+    onChange,
+    ...rest
+  } = props;
+
+  let input = textarea ? (
+    <textarea
+      rows={10}
+      type={type}
+      className={clsx(
+        "bg-[#1A2238] px-[16px] py-[18px] w-full rounded-lg outline-[#232F52] placeholder-[#A7AFC7] text-[#DFE5F8] text-[16px]",
+        className,
+        errors && errors[id] && "input-error"
+      )}
+      id={id}
+      value={value}
+      onChange={onChange}
+      {...rest}
+      {...(register && register(id))}
+    />
+  ) : (
+    <input
+      type={type}
+      className={clsx(
+        "bg-[#1A2238] px-[16px] py-[18px] w-full rounded-lg outline-[#232F52] placeholder-[#A7AFC7] text-[#DFE5F8] text-[16px]",
+        className,
+        errors && errors[id] && "input-error"
+      )}
+      id={id}
+      value={value}
+      onChange={onChange}
+      {...rest}
+      {...(register && register(id))}
+    />
+  );
 
   return (
     <>
-      <input
-        type={type}
-        className={clsx(
-          "bg-[#1A2238] px-[16px] py-[18px] w-full rounded-lg outline-[#232F52] placeholder-[#A7AFC7] text-[#DFE5F8] text-[16px]",
-          className,
-          errors && errors[id] && "input-error"
-        )}
-        id={id}
-        value={value}
-        {...rest}
-        {...(register && register(id))}
-      />
+      {input}
       {errors && errors[id] && <ErrorLabel>{errors[id].message}</ErrorLabel>}
     </>
   );
