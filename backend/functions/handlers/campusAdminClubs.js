35379ae46944ebe848af89ab6441f772425c10f5
rajashilan <rajashilan07@gmail.com>
2023-07-22 17:55:52 +0800

    feat(backend): clubs admin functionalities

---
+++ backend/functions/handlers/campusAdminClubs.js
@@ -0,0 +1,159 @@
+const { admin, db } = require("../utils/admin");
+const firebase = require("firebase");
+const crypto = require("crypto");
+const { FieldValue } = require("firebase-admin/firestore");
+
+//get all clubs
+exports.getAllClubs = (req, res) => {
+  const campusID = req.params.campusID;
+
+  db.collection("clubs")
+    .where("campusID", "==", campusID)
+    .get()
+    .then((data) => {
+      let clubs = [];
+      data.forEach((doc) => {
+        clubs.push(...doc.data());
+      });
+      return res.status(200).json(clubs);
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).json({ error: "Something went wrong" });
+    });
+};
+
+//approve a club
+exports.approveClub = (req, res) => {
+  const clubID = req.params.clubID;
+  const campusID = req.params.campusID;
+  const createdBy = req.body.createdBy;
+  const rejectionReason = "";
+
+  //have to update in clubs, clubsOverview, and in Users
+
+  //for users, the only logic is if the club approval is pending, approved, or rejected
+  //so we'll only have to access user's data when it comes to this part
+  //and we can access it using the createdBy since only the createdBy user will be in the club
+
+  db.doc(`/users/${createdBy}`)
+    .get()
+    .then((doc) => {
+      let temp = doc.data().clubs;
+      let index = temp.findIndex((club) => clubID === clubID);
+      temp[index].approval = "approved";
+
+      return db.doc(`/users/${createdBy}`).update({ clubs: [...temp] });
+    })
+    .then(() => {
+      return db
+        .doc(`/clubs/${clubID}`)
+        .update({ approval: "approved", rejectionReason });
+    })
+    .then(() => {
+      return db
+        .collection(`/clubsOverview/${campusID}`)
+        .update({ approval: "approved", rejectionReason });
+    })
+    .then(() => {
+      return res.status(200).json({ clubID });
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).json({ error: "Something went wrong" });
+    });
+};
+
+//reject a club
+exports.rejectClub = (req, res) => {
+  const clubID = req.params.clubID;
+  const campusID = req.params.campusID;
+  const createdBy = req.body.createdBy;
+  const rejectionReason = req.body.rejectionReason;
+
+  //have to update in clubs, clubsOverview, and in Users
+
+  //for users, the only logic is if the club approval is pending, approved, or rejected
+  //so we'll only have to access user's data when it comes to this part
+  //and we can access it using the createdBy since only the createdBy user will be in the club
+
+  db.doc(`/users/${createdBy}`)
+    .get()
+    .then((doc) => {
+      let temp = doc.data().clubs;
+      let index = temp.findIndex((club) => clubID === clubID);
+      temp[index].approval = "rejected";
+
+      return db.doc(`/users/${createdBy}`).update({ clubs: [...temp] });
+    })
+    .then(() => {
+      return db
+        .doc(`/clubs/${clubID}`)
+        .update({ approval: "rejected", rejectionReason });
+    })
+    .then(() => {
+      return db
+        .collection(`/clubsOverview/${campusID}`)
+        .update({ approval: "rejected", rejectionReason });
+    })
+    .then(() => {
+      return res.status(200).json({ clubID });
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).json({ error: "Something went wrong" });
+    });
+};
+
+//suspend a club
+//indefinetely or for a time period
+//time period in terms of days, starting from timestamp
+//so, each time club is opened, check for suspension and timestamp
+//if present, and if passed the time period
+//remove suspension and update in db from app
+exports.suspendClub = (req, res) => {
+  const clubID = req.params.clubID;
+  const suspension = req.body.suspension;
+
+  //format:
+  //suspension:indefinetely
+  //suspension:timestampInMillis:7 -> days
+
+  db.doc(`/clubs/${clubID}`)
+    .update({ status: suspension })
+    .then(() => {
+      return db
+        .collection(`/clubsOverview/${campusID}`)
+        .update({ status: suspension });
+    })
+    .then(() => {
+      return res.status(200).json({ clubID });
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).json({ error: "Something went wrong" });
+    });
+};
+
+//lift club's suspension
+exports.removeSuspension = (req, res) => {
+  const clubID = req.params.clubID;
+  const status = "active";
+
+  //format:
+  //suspension:indefinetely
+  //suspension:timestampInMillis:7 -> days
+
+  db.doc(`/clubs/${clubID}`)
+    .update({ status })
+    .then(() => {
+      return db.collection(`/clubsOverview/${campusID}`).update({ status });
+    })
+    .then(() => {
+      return res.status(200).json({ clubID });
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).json({ error: "Something went wrong" });
+    });
+};
