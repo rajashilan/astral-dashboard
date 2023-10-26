e2d6ecac0adbc9d7615c748006e74a0267e8a161
rajashilan <rajashilan07@gmail.com>
2023-10-27 00:48:52 +0800

    feat(Navbar): implements dynamic navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,39 +1,123 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.svg";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../redux/actions/userActions";
 
 function Navbar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const state = useSelector((state) => state.user.adminData);
+  const loading = useSelector((state) => state.UI.loading);
+
   const [authorized, setAuthorized] = useState(false);
   const navActive = "font-bold text-[#C4FFF9] underline text-[1.125rem]";
   const navInactive = "font-normal text-[#DFE5F8] text-[1.125rem]";
-  const hiddenClass = authorized
+  const hiddenClass = state.role
     ? "sticky top-0 h-[5.75rem] flex flex-row w-full bg-[#0C111F] px-[5rem] justify-between items-center"
     : "hidden";
 
+  useEffect(() => {
+    if (!loading) {
+      if (state.role) {
+        return;
+      } else navigate("/login");
+    }
+  }, [loading]);
+
+  let menu = loading ? (
+    <p>loading...</p>
+  ) : state.role && state.role[0] === "sudo" ? (
+    <div className="flex flex-row space-x-[2.875rem]">
+      <Link to="/college">
+        <p
+          className={
+            window.location.pathname === "/college" ? navActive : navInactive
+          }
+        >
+          college
+        </p>
+      </Link>
+      <Link to="/orientation">
+        <p
+          className={
+            window.location.pathname === "/orientation"
+              ? navActive
+              : navInactive
+          }
+        >
+          orientation
+        </p>
+      </Link>
+      <Link to="/clubs">
+        <p
+          className={
+            window.location.pathname === "/clubs" ? navActive : navInactive
+          }
+        >
+          clubs
+        </p>
+      </Link>
+      <button
+        onClick={() => dispatch(logoutUser())}
+        className="font-normal text-gray-400 text-[1.125rem]"
+      >
+        logout
+      </button>
+    </div>
+  ) : (
+    <div className="flex flex-row space-x-[2.875rem]">
+      {state.role && state.role.includes("focused:college") && (
+        <Link to="/college">
+          <p
+            className={
+              window.location.pathname === "/college" ? navActive : navInactive
+            }
+          >
+            college
+          </p>
+        </Link>
+      )}
+      {state.role && state.role.includes("focused:orientation") && (
+        <Link to="/orientation">
+          <p
+            className={
+              window.location.pathname === "/orientation"
+                ? navActive
+                : navInactive
+            }
+          >
+            orientation
+          </p>
+        </Link>
+      )}
+      {state.role && state.role.includes("focused:clubs") && (
+        <Link to="/clubs">
+          <p
+            className={
+              window.location.pathname === "/clubs" ? navActive : navInactive
+            }
+          >
+            clubs
+          </p>
+        </Link>
+      )}
+      <button
+        onClick={() => dispatch(logoutUser())}
+        className="font-normal text-gray-400 text-[1.125rem]"
+      >
+        logout
+      </button>
+    </div>
+  );
+
   return (
     <div className={hiddenClass}>
-      <img src={logo} alt="astral" className="w-[7rem] h-[3.25rem]" />
-      <div className="flex flex-row space-x-[2.875rem]">
-        <a href="#" className={navActive}>
-          college
-        </a>
-        <a href="#" className={navInactive}>
-          orientation
-        </a>
-        <a href="#" className={navInactive}>
-          clubs
-        </a>
-        <a href="#" className={navInactive}>
-          departments
-        </a>
-        <a href="#" className={navInactive}>
-          staff list
-        </a>
-        <div className="!ml-[4rem]">
-          <a href="#" className={navInactive}>
-            Darmian
-          </a>
-        </div>
-      </div>
+      <Link to="/">
+        <img src={logo} alt="astral" className="w-[7rem] h-[3.25rem]" />
+      </Link>
+      {menu}
     </div>
   );
 }
