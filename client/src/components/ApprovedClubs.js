c06c95d7d7641438548a7b2e81176610773e3e58
rajashilan <rajashilan07@gmail.com>
2024-01-27 02:05:30 +0800

    fix(ApprovedClubs): fixes className error

--- client/src/components/ApprovedClubs.js
+++ client/src/components/ApprovedClubs.js
@@ -182,7 +182,7 @@ export default function ApprovedClubs() {
           <select
             disabled={manageModalLoading}
             id="members"
-            class="mt-[14px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="mt-[14px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             onChange={(e) => {
               let index = clubMembers.findIndex(
                 (member) => member.userID === e.target.value
