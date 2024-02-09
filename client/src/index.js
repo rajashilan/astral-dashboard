d0a34e8bb9856619ba283c5c7af670aa780ecc28
rajashilan <rajashilan07@gmail.com>
2024-02-09 20:14:27 +0800

    feat: setinterval to check auth every 5 mins

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,36 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+import { logoutUser, getSessionData } from "./redux/actions/userActions";
+import { SET_AUTHENTICATED } from "./redux/types";
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser());
+  } else {
+    store.dispatch({
+      type: SET_AUTHENTICATED,
+    });
+    axios.defaults.headers.common["Authorization"] = token;
+    store.dispatch(getSessionData());
+  }
+}
+
+// Periodically check token validity every 5 minutes
+setInterval(() => {
+  const token = localStorage.FBIdToken;
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    if (decodedToken.exp * 1000 < Date.now()) {
+      store.dispatch(logoutUser());
+    }
+  }
+}, 5 * 60 * 1000);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
